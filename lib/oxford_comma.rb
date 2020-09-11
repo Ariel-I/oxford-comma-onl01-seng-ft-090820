6c881904a55933d8e4d3ef112b9491873a63564e
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 22:05:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,4 @@
-def oxford_comma(array=["kiwi", "durian"])
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
+
 
 def oxford_comma(array=["kiwi", "durian", "mango"])
   if array.length == 1 
@@ -16,7 +6,9 @@ def oxford_comma(array=["kiwi", "durian", "mango"])
     elsif array.length == 2 
     array.join(" and ")
     elsif array.length > 2
-    array[-1].prepend
+    array[-1].prepend "and"
+    array.join (" , ")
+  end
 
 
 
