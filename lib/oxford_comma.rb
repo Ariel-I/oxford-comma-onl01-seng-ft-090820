a79c14c563d8b736467e3433214c2c1988a8f657
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 22:00:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array= ["kiwi", "durian"])
- if array.length == 1 ["kiwi"].join
+ if array.length == 1 
+   ["kiwi"].join
+ elsif array.length == 2
   ["kiwi", "durian"].join(" and ")
  
 end
