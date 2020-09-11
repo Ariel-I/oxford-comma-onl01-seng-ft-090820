87f26f342a5aeb4685a2946dd0a1cd34df061360
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 22:08:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 
 
 def oxford_comma(array=["kiwi", "durian", "mango"])
-  if array.length == 1 
-    array.join
-    elsif array.length == 2 
-    array[-2]<< " and "
+  if array.length == 2 
+    array[-2] << " and "
     arra.join
-    elsif array.length > 2
+  elsif array.length == 1 
+    array.join
+  elsif array.length > 2
     array[-1].prepend "and"
     array.join (" , ")
   end
